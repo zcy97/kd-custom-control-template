3668ad1a881ccad68e309c691ef28ba8233f3de3
zcy97 <1050365185@qq.com>
2024-09-18 15:40:31 +0800

    vue3-noui template commit

--- src/components/App.vue
+++ src/components/App.vue
@@ -3,7 +3,6 @@
 <template>
   <div id="app">
     <h1>Vue3 自定义控件</h1>
-    <el-button type="primary">Primary</el-button>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -30,15 +27,7 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
-    plugins: [
-      vue(),
-      AutoImport({
-        resolvers: [ElementPlusResolver()],
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()],
-      }),
-    ],
+    plugins: [vue()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
